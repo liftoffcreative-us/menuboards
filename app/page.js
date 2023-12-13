0f8991a7330a7184904bfd4efa5bf42d66002c2f
Liftoff Creative <info@lifoffcreative.us>
2023-12-13 14:13:34 -0600

    update landing page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import ezLogo from '../public/ezLogo.png'
+import board2 from '../public/board2.png'
 import board1 from '../public/board1.png'
 import Link from 'next/link'
 
@@ -18,7 +18,7 @@ export default function Home() {
                 <h2 className='text-2xl'>Menu Board 1</h2>
               </div>
               <div className='flex flex-col w-1/3 shadow-md  items-center justify-center bg-white rounded-md px-6 py-6 text-gray-600'>
-                <Link href="/board2" className='mb-4 border border-gray-300 rounded-xl overflow-hidden'><Image src={board1} alt="Menu Board 1"   /> </Link>
+                <Link href="/board2" className='mb-4 border border-gray-300 rounded-xl overflow-hidden'><Image src={board2} alt="Menu Board 2"   /> </Link>
                 <h2 className='text-2xl'>Menu Board 2</h2>
               </div>
           </div>
