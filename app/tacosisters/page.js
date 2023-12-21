4135b30d4f0a12acd89f79ee0a6abfc10756c73d
Liftoff Creative <info@lifoffcreative.us>
2023-12-21 16:12:00 -0600

    fix sizing issues

--- app/tacosisters/page.js
+++ app/tacosisters/page.js
@@ -11,7 +11,7 @@ export default function Home() {
     <main className="flex flex-col min-h-screen min-w-screen bg-[#e6e6e6]">
       <div className='flex items-center justify-between w-full h-[10vh]  pl-6 '>
         <Image src={ezmenu} alt="ezmenuboard logo" className='h-5/6 w-auto' />
-        <div className='flex items-center gap-2 text-2xl mx-6 text-ezblue'>
+        <div className='flex items-center gap-2 text-[1rem] mx-6 text-ezblue'>
           <h1 >Taco Sisters</h1>
           <FaUserCircle/>
         </div>
